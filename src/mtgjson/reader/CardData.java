a30229510ae0b20a34a1aae4b95f035b92553bc1
Lodici <Lodici>
2014-05-08 17:49:19 +0100

    initial commit.

---
+++ src/mtgjson/reader/CardData.java
@@ -0,0 +1,155 @@
+/**
+ *
+ */
+package mtgjson.reader;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import org.apache.commons.lang3.text.WordUtils;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+class CardData {
+
+    private final JsonObject jsonCard;
+
+    private String cardName;
+    private String infoUrl;
+    private String imageUrl;
+    private String rarity;
+    private String manaCost;
+    private String type;
+    private String subType      = null;
+    private String power        = null;
+    private String toughness    = null;
+    private String text         = null;
+
+    public CardData(final JsonObject card) throws UnsupportedEncodingException {
+
+        this.jsonCard = card;
+
+        setCardName(card.get("name").getAsString());
+
+        setInfoUrl("http://magiccards.info/query?q=%21" + URLEncoder.encode(card.get("name").getAsString(), "UTF-8"));
+        setImageUrl("http://mtgimage.com/card/" + URLEncoder.encode(card.get("imageName").getAsString(), "UTF-8").replace("+", "%20") + ".jpg");
+        setRarity(card.get("rarity").getAsString().substring(0,  1) + " #" + card.get("rarity").getAsString());
+
+        if (card.has("manaCost"))   { setManaCost(card.get("manaCost").getAsString()); }
+        if (card.has("power"))      { setPower(card.get("power").getAsString()); }
+        if (card.has("toughness"))  { setToughness(card.get("toughness").getAsString()); }
+        if (card.has("text"))       { setText(card.get("text").getAsString()); }
+
+        if (card.has("types")) {
+            final StringBuffer sb = new StringBuffer();
+            final JsonArray cardTypes = card.getAsJsonArray("types");
+            for (int j = 0; j < cardTypes.size(); j++) {
+                sb.append(cardTypes.get(j).toString().replace("\"", "")).append(",");
+            }
+            final String typeValues = sb.toString().substring(0, sb.toString().length() - 1);
+            setType(typeValues);
+        }
+
+        if (card.has("subtypes")) {
+            final StringBuffer sb = new StringBuffer();
+            final JsonArray cardTypes = card.getAsJsonArray("subtypes");
+            for (int j = 0; j < cardTypes.size(); j++) {
+                final String subType = cardTypes.get(j).toString();
+                sb.append(subType
+                            .replace("\"", "")
+                            .replace(" ", "_")
+                            .replace("'s", "")
+                            .replace("’s", "")
+                            .replace("-", "_")).append(",");
+            }
+            final String typeValues = sb.toString().substring(0, sb.toString().length() - 1);
+            setSubType(typeValues);
+        }
+
+    }
+
+    public String getCardName(final boolean replaceNonAscii) {
+        if (replaceNonAscii) {
+            return cardName.replace("Æ", "AE");
+        } else {
+            return cardName;
+        }
+    }
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getInfoUrl() {
+        return infoUrl;
+    }
+    public void setInfoUrl(String infoUrl) {
+        this.infoUrl = infoUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getManaCost() {
+        return manaCost;
+    }
+    public void setManaCost(String manaCost) {
+        this.manaCost = manaCost;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFilename() {
+        String filename = null;
+        if (jsonCard.has("imageName")) {
+            filename = jsonCard.get("imageName").getAsString();
+        } else {
+            filename = getCardName(true);
+        }
+        return WordUtils.capitalize(filename).replace(" ", "_") + ".txt";
+    }
+
+    public String getPower() {
+        return power;
+    }
+    public void setPower(String power) {
+        this.power = power;
+    }
+
+    public String getToughness() {
+        return toughness;
+    }
+    public void setToughness(String toughness) {
+        this.toughness = toughness;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSubType() {
+        return subType;
+    }
+    public void setSubType(String subType) {
+        this.subType = subType;
+    }
+
+}
---
+++ src/mtgjson/reader/MtgJsonReader.java
@@ -0,0 +1,282 @@
+/**
+ *
+ */
+package mtgjson.reader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
+/**
+ * Given a list of missing card names from Magarena this will attempt to match each
+ * card name to an entry in the json feed from mgtjson.com and create a script file
+ * for each matching card which can be loaded into MagicCardDefinition for display
+ * in the Card Explorer screen.
+ *
+ */
+public class MtgJsonReader {
+
+    // This file is obtained from mtgjson.com. It contains every card grouped by set.
+    private static final String JSON_FILE = "AllSets.json";
+
+    // This is a list of the cards which have not yet been implemented in Magarena.
+    // In effect it is the list of cards in AllCardNames.txt minus those cards which
+    // have a matching script file in the "cards" folder.
+    // !! NB I hacked some code in Magarena to create this (not supplied) !!
+    private static final String MISSING_CARDS_FILE = "CardsMissingInMagarena.txt";
+
+    // This contains entries from CardsMissingInMagarena.txt which have no matching entry in
+    // the mtgcom json ("AllSets.json") file. Check the name for typos, strange characters, etc.
+    private static final String MISSING_ORPHANS_FILE = "MissingCardOrphans.txt";
+
+    private static final List<String> setCodesList = new ArrayList<String>();
+    private static final HashMap<String, CardData> mtgcomCards = new HashMap<String, CardData>();
+    private static final List<String> magarenaMissingCards = new ArrayList<String>();
+
+    public static void main(String[] args) throws IOException {
+
+        loadJsonData();
+        System.out.println("Total unique cards identified in json feed = " + mtgcomCards.size() + " (see " + JSON_FILE + ")");
+
+        loadMissingMagarenaCards();
+        System.out.println("Total missing cards in Magarena = " + magarenaMissingCards.size() + " (see " + MISSING_CARDS_FILE + ")");
+
+        // sort list of ALL card names from json file.
+        final List<String> mtgcomCardNames = new ArrayList<String>(mtgcomCards.keySet());
+        Collections.sort(mtgcomCardNames);
+
+        final int missingOrphans = saveListOfMissingCardOrphans(mtgcomCardNames);
+        System.out.println("Total missing cards which could not be matched in " + JSON_FILE + " = " + missingOrphans + " (see " + MISSING_ORPHANS_FILE + ")");
+
+        mtgcomCardNames.retainAll(magarenaMissingCards);
+        System.out.println("Total missing cards which have a matching entry in " + JSON_FILE + " = " + magarenaMissingCards.size() + "-" + missingOrphans + " = " + mtgcomCardNames.size() + " (see MissingCardsWithFilenames.txt)");
+
+        saveMissingCardNamesWithFilenames(mtgcomCardNames);
+        System.out.println("Saved MissingCardsWithFilenames.txt");
+
+        saveMissingCardData(mtgcomCardNames);
+        System.out.println("Created a default script file for each missing card in \"scripts\" folder.");
+
+        System.out.println("Finished.");
+
+    }
+
+    /**
+     * Loads the data from JSON_FILE using google gson library.
+     * <p>
+     * The data is stored by card set so in order to get all cards you have to
+     * step through each set and pick out any new unique cards.
+     */
+    private static void loadJsonData() throws IOException {
+
+        // Explicitly state UTF-8 otherwise will get strange characters if the default
+        // encoding is different (which it seems to be on Windows 7 at least).
+        try (final BufferedReader reader =
+                new BufferedReader(
+                        new InputStreamReader(
+                                new FileInputStream(JSON_FILE), "UTF-8"))) {
+
+            final JsonParser parser = new JsonParser();
+            final JsonElement element = parser.parse(reader);
+
+            for (String setCode : getSetCodes()) {
+                // Not interested in unsets.
+                if (!setCode.equalsIgnoreCase("UNH")) {
+                    final JsonObject setObject = element.getAsJsonObject().get(setCode).getAsJsonObject();
+                    extractCardDataFromJson(setObject.getAsJsonArray("cards"));
+                }
+            }
+
+        }
+
+    }
+
+    /**
+     * Saves a list of the card names that are present in the missing cards list from
+     * Magarena but which have no matching card name in the json file from mtgjson.com.
+     */
+    private static int saveListOfMissingCardOrphans(final List<String> mtgcomCardNames) {
+        final List<String> missingCardOrphans = new ArrayList<String>(magarenaMissingCards);
+        missingCardOrphans.removeAll(mtgcomCardNames);
+        Collections.sort(missingCardOrphans);
+        try (final PrintWriter writer = new PrintWriter("results" + File.separator + MISSING_ORPHANS_FILE)) {
+            for (String cardName : missingCardOrphans) {
+                writer.println(cardName);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        };
+        return missingCardOrphans.size();
+    }
+
+    private static void extractCardDataFromJson(final JsonArray cards) throws UnsupportedEncodingException {
+        for (int i = 0; i < cards.size(); i++) {
+            final CardData cardData = new CardData(cards.get(i).getAsJsonObject());
+            if (!mtgcomCards.containsKey(cardData.getCardName(true))) {
+                mtgcomCards.put(cardData.getCardName(true), cardData);
+            };
+        }
+    }
+
+    private static void saveMissingCardData(final List<String> cardNames) {
+        for (String cardName : cardNames) {
+            final CardData cardData = mtgcomCards.get(cardName);
+            saveCardData(cardData);
+        }
+    }
+
+    private static void saveCardData(final CardData cardData) {
+        final Path scriptsPath = Paths.get("results").resolve("scripts");
+        if (!Files.isDirectory(scriptsPath)) {
+            try {
+                Files.createDirectory(scriptsPath);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        final Path filePath = scriptsPath.resolve(cardData.getFilename());
+        try (final PrintWriter writer = new PrintWriter(filePath.toString())) {
+            writer.println("name=" + cardData.getCardName(false));
+            writer.println("url=" + cardData.getInfoUrl());
+            writer.println("image=" + cardData.getImageUrl());
+            writer.println("rarity=" + cardData.getRarity());
+            writer.println("type=" + cardData.getType());
+            if (cardData.getSubType() != null) {
+                writer.println("subtype=" + cardData.getSubType());
+            }
+            if (cardData.getManaCost() != null) {
+                writer.println("cost=" + cardData.getManaCost());
+            }
+            if (cardData.getPower() != null && cardData.getToughness() != null) {
+                writer.println("pt=" + cardData.getPower() + "/" + cardData.getToughness());
+            }
+            if (cardData.getText() != null) {
+                writer.println("text=" + cardData.getText().replace("\n", "\\n"));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        };
+    }
+
+    private static void saveMissingCardNamesWithFilenames(final List<String> cardNames) {
+        Collections.sort(cardNames);
+        try (final PrintWriter writer = new PrintWriter("results/MissingCardsWithFilenames.txt")) {
+            for (String cardName : cardNames) {
+                final CardData cardData = mtgcomCards.get(cardName);
+                writer.println(cardName + "-->" + cardData.getFilename());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        };
+    }
+
+    private static String[] getSetCodes() throws IOException {
+        FileReader fileReader = new FileReader(JSON_FILE);
+        JsonReader reader = new JsonReader(fileReader);
+        handleObject(reader);
+        return setCodesList.toArray(new String[setCodesList.size()]);
+    }
+
+    /**
+     * Handle an Object. Consume the first token which is BEGIN_OBJECT. Within
+     * the Object there could be array or non array tokens. We write handler
+     * methods for both. Noe the peek() method. It is used to find out the type
+     * of the next token without actually consuming it.
+     *
+     * @param reader
+     * @throws IOException
+     */
+    private static void handleObject(JsonReader reader) throws IOException {
+        reader.beginObject();
+        while (reader.hasNext()) {
+            JsonToken token = reader.peek();
+            if (token.equals(JsonToken.BEGIN_ARRAY))
+                handleArray(reader);
+            else if (token.equals(JsonToken.END_ARRAY)) {
+                reader.endObject();
+                return;
+            } else
+                handleNonArrayToken(reader, token);
+        }
+
+    }
+
+    /**
+     * Handle a json array. The first token would be JsonToken.BEGIN_ARRAY.
+     * Arrays may contain objects or primitives.
+     *
+     * @param reader
+     * @throws IOException
+     */
+    public static void handleArray(JsonReader reader) throws IOException {
+        reader.beginArray();
+        while (true) {
+            JsonToken token = reader.peek();
+            if (token.equals(JsonToken.END_ARRAY)) {
+                reader.endArray();
+                break;
+            } else if (token.equals(JsonToken.BEGIN_OBJECT)) {
+                handleObject(reader);
+            } else
+                handleNonArrayToken(reader, token);
+        }
+    }
+
+    /**
+     * Handle non array non object tokens
+     *
+     * @param reader
+     * @param token
+     * @throws IOException
+     */
+    public static void handleNonArrayToken(JsonReader reader, JsonToken token) throws IOException {
+        if (token.equals(JsonToken.NAME)) {
+            final String value = reader.nextName();
+            setCodesList.add(value);
+        } else if (token.equals(JsonToken.STRING)) {
+            System.out.println(reader.nextString());
+        } else if (token.equals(JsonToken.NUMBER)) {
+            System.out.println(reader.nextDouble());
+        } else {
+            reader.skipValue();
+        }
+    }
+
+    /**
+     *  sorted list of missing card names from Magarena.
+     */
+    public static void loadMissingMagarenaCards() {
+        magarenaMissingCards.clear();
+        try {
+            final Path filePath = Paths.get(MISSING_CARDS_FILE);
+            for (final String cardName : Files.readAllLines(filePath, Charset.defaultCharset())) {
+                magarenaMissingCards.add(cardName.trim());
+            }
+        } catch (final IOException ex) {
+           throw new RuntimeException(ex);
+        }
+    }
+
+}
