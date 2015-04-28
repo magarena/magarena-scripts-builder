4b4dd38f5365580a4564d8f71e54034c08225e9b
ShawnieBoy <ShawnieBoyUK@gmail.com>
2015-04-28 12:19:30 +0100

    fix timing logic + cleanup

--- src/mtgjson/reader/CardData.java
+++ src/mtgjson/reader/CardData.java
@@ -1,15 +1,39 @@
 package mtgjson.reader;
 
 import java.io.UnsupportedEncodingException;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import java.util.SortedMap;
 import java.util.TreeMap;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 class CardData {
 
     public static final SortedMap<String, String> cardImageErrors = new TreeMap<>();
 
+    private static void addCardImageError(final CardData card, final String errorDetails) {
+        final String key = card.getCardName();
+        if (!cardImageErrors.containsKey(key)) {
+            cardImageErrors.put(key, errorDetails);
+        }
+    }
+    private static void clearCardImageError(final CardData card) {
+        final String key = card.getCardName();
+        if (cardImageErrors.containsKey(key)) {
+            cardImageErrors.remove(key);
+        }
+    }
+    public static String getId(final JsonObject card) {
+        return card.get("name").getAsString().trim();
+    }
+    public static String getRarity(final JsonObject card) {
+        return card.get("rarity").getAsString().substring(0, 1);
+    }
+    public static boolean isValid(final JsonObject jsonCard) {
+        return !CardData.getRarity(jsonCard).contentEquals("S") &&
+                (jsonCard.has("number") || jsonCard.has("multiverseid"));
+    }
+    
     private String cardName;
     private String imageUrl = "";
     private String rarity;
@@ -52,6 +76,146 @@ class CardData {
         }
     }
 
+    private void extractAbilityText(final JsonObject json) {
+        abilityText = json.get("text").getAsString()
+                .replaceAll("^\\(.+?\\)\n", "")
+                .replaceAll("^.+— ", "")
+                .replace("\n", ";\\\n        ")
+                .replace(";\\\n        •", " \\\n        •")
+                .replaceAll(" \\(.+?\\)", "")
+                .replaceFirst(" •", " (1)")
+                .replaceFirst(" •", " (2)")
+                .replaceFirst(" •", " (3)")
+                .replaceFirst(" •", " (4)")
+                .replace(getCardName(), "SN");
+    }
+
+    private void extractCardName(final JsonObject json) {
+        cardName = json.get("name").getAsString();
+    }
+
+    private void extractColor(final JsonObject json) {
+        if (json.has("colors") && json.has("manaCost") == false) {
+            color = json.get("colors")
+                    .toString()
+                    .replace("Blue", "U")
+                    .replaceAll("\\W", "")
+                    .replaceAll("[a-z]", "")
+                    .toLowerCase();
+        }
+    }
+
+    private void extractEffectText(final JsonObject json) {
+        effectText = json.get("text").getAsString()
+                .replaceAll("^\\(.+?\\)\n", "")
+                .replaceAll("^.+— ", "")
+                .replace("\n", "~")
+                .replaceAll("~.+? — ", "~")
+                .replaceAll(" \\(.+?\\)", "")
+                .replaceAll("\n~•", "~•")
+                .replaceFirst("~•", " (1)")
+                .replaceFirst("~•", " (2)")
+                .replaceFirst("~•", " (3)")
+                .replaceFirst("~•", " (4)")
+                .replace(getCardName(), "SN");
+    }
+
+    private void extractImageUrl(final JsonObject json) throws UnsupportedEncodingException {
+
+        if (json.has("number")) {
+            imageUrl = String.format(
+                    "http://magiccards.info/scans/en/%s/%s.jpg",
+                    setCode.toLowerCase(),
+                    json.get("number").getAsString()
+                    );
+            CardData.clearCardImageError(this);
+
+        } else if (json.has("multiverseid")) {
+            imageUrl = setCode.toLowerCase();
+            CardData.clearCardImageError(this);
+
+        } else {
+            CardData.addCardImageError(this, String.format("%s has no number or multiverseid - cannot set {image} property.", cardName));
+        }
+
+    }
+
+    private void extractManaCost(final JsonObject json) {
+        if (json.has("manaCost")) {
+            manaCost = json.get("manaCost").getAsString();
+        }
+    }
+
+    private void extractOracleText(final JsonObject json) {
+        if (json.has("text")) {
+            oracleText = json.get("text").getAsString()
+                    .replaceAll("^\\(.+?\\)\n", "")
+                    .replaceAll(" \\(.+?\\)", "")
+                    .replaceAll("\n","\\\\n")
+                    .replaceAll(";",",");
+        }
+    }
+
+    private void extractPower(final JsonObject json) {
+        if (json.has("power")) {
+            power = json.get("power").getAsString();
+        }
+    }
+
+    private void extractRarity(final JsonObject json) {
+        rarity = CardData.getRarity(json);
+    }
+
+    private void extractSubTypes(final JsonObject json) {
+        if (json.has("subtypes")) {
+            final StringBuffer sb = new StringBuffer();
+            final JsonArray cardTypes = json.getAsJsonArray("subtypes");
+            for (int j = 0; j < cardTypes.size(); j++) {
+                final String subType = cardTypes.get(j).toString();
+                sb.append(subType
+                        .replace("\"", "")
+                        .replace(" ", "_")
+                        .replace("’s", "'s")
+                        .replace("-", "_")).append(",");
+            }
+            final String typeValues = sb.toString().substring(0, sb.toString().length() - 1);
+            subTypes = typeValues;
+        }
+    }
+
+    private void extractSuperTypes(final JsonObject json) {
+        if (json.has("supertypes")) {
+            final StringBuffer sb = new StringBuffer();
+            final JsonArray cardTypes = json.getAsJsonArray("supertypes");
+            for (int j = 0; j < cardTypes.size(); j++) {
+                sb.append(cardTypes.get(j).toString().replace("\"", "")).append(",");
+            }
+            final String superTypes =sb.toString().substring(0,sb.toString().length()-1);
+            superType = superTypes;
+        }
+    }
+
+    private void extractToughness(final JsonObject json) {
+        if (json.has("toughness")) {
+            toughness = json.get("toughness").getAsString();
+        }
+    }
+
+    private void extractTypes(final JsonObject json) {
+        if (json.has("types")) {
+            final StringBuffer sb = new StringBuffer();
+            final JsonArray cardTypes = json.getAsJsonArray("types");
+            for (int j = 0; j < cardTypes.size(); j++) {
+                sb.append(cardTypes.get(j).toString().replace("\"", "")).append(",");
+            }
+            final String typeValues = sb.toString().substring(0, sb.toString().length() - 1);
+            type = typeValues;
+        }
+    }
+    public String getAbilityText() {
+        return abilityText;
+    }
+
     public String getCardName() {
         return cardName;
     }
@@ -68,46 +232,72 @@ class CardData {
                 .replace("ú", "_")
                 .replace("û", "_");
     }
-    
-    private void extractCardName(final JsonObject json) {
-        this.cardName = json.get("name").getAsString();
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getEffectText() {
+        return effectText;
+    }
+
+    public String getFilename() {
+        return getCardNameAsAscii().replaceAll("[^A-Za-z0-9]", "_") + ".txt";
     }
 
     public String getImageUrl() {
         return imageUrl;
     }
 
-    public String getRarity() {
-        return rarity;
-    }
-
-    private void extractRarity(final JsonObject json) {
-        this.rarity = getRarity(json);
-    }
-
     public String getManaCost() {
         return manaCost;
     }
 
-    private void extractManaCost(final JsonObject json) {
-        if (json.has("manaCost")) {
-            this.manaCost = json.get("manaCost").getAsString();
-        }
+    public String getOracleText() {
+        return oracleText;
     }
 
-    public String getColor() {
-        return color;
+    public String getPower() {
+        return power;
     }
 
-    private void extractColor(final JsonObject json) {
-        if (json.has("colors") && json.has("manaCost") == false) {
-            this.color = json.get("colors")
-                    .toString()
-                    .replace("Blue", "U")
-                    .replaceAll("\\W", "")
-                    .replaceAll("[a-z]", "")
-                    .toLowerCase();
+    public String getRarity() {
+        return rarity;
+    }
+
+    public String getSubType() {
+        return subTypes;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTiming() {
+        if (type.contains("Instant")) {
+            return "removal";
+        } 
+        if (type.contains("Land")){
+            return "land";
         }
+        if (hasSubType()) {
+            if (subTypes.contains("Equipment")) {
+                return "equipment";
+            } if (subTypes.contains("Aura")) {
+                return "aura";
+            }
+        }
+        if (type.contains("Artifact")) {
+            return "artifact";
+        }
+        if (type.contains("Enchantment")) {
+            return "enchantment";
+        }
+        return "main";
+    }
+
+    public String getToughness() {
+        return toughness;
     }
 
     public String getType() {
@@ -117,223 +307,37 @@ class CardData {
             return type;
         }
     }
-    
-    private void extractTypes(final JsonObject json) {
-        if (json.has("types")) {
-            final StringBuffer sb = new StringBuffer();
-            final JsonArray cardTypes = json.getAsJsonArray("types");
-            for (int j = 0; j < cardTypes.size(); j++) {
-                sb.append(cardTypes.get(j).toString().replace("\"", "")).append(",");
-            }
-            final String typeValues = sb.toString().substring(0, sb.toString().length() - 1);
-            this.type = typeValues;
-        }
-    }
 
-    public String getFilename() {
-        return getCardNameAsAscii().replaceAll("[^A-Za-z0-9]", "_") + ".txt";
-    }
-
-    public String getPower() {
-        return power;
-    }
-
-    private void extractPower(final JsonObject json) {
-        if (json.has("power")) {
-            this.power = json.get("power").getAsString();
-        }
-    }
-
-    public String getToughness() {
-        return toughness;
-    }
-    
-    private void extractToughness(final JsonObject json) {
-        if (json.has("toughness")) {
-            this.toughness = json.get("toughness").getAsString();
-        }
-    }
-
-    public String getText() {
-        return text;
-    }
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    private void extractSuperTypes(final JsonObject json) {
-        if (json.has("supertypes")) {
-            final StringBuffer sb = new StringBuffer();
-            final JsonArray cardTypes = json.getAsJsonArray("supertypes");
-            for (int j = 0; j < cardTypes.size(); j++) {
-                sb.append(cardTypes.get(j).toString().replace("\"", "")).append(",");
-            }
-            final String superTypes =sb.toString().substring(0,sb.toString().length()-1);
-            this.superType = superTypes;
-        }
-    }
-
-    public String getSubType() {
-        return subTypes;
-    }
-    
-    private void extractSubTypes(final JsonObject json) {
-        if (json.has("subtypes")) {
-            final StringBuffer sb = new StringBuffer();
-            final JsonArray cardTypes = json.getAsJsonArray("subtypes");
-            for (int j = 0; j < cardTypes.size(); j++) {
-                final String subType = cardTypes.get(j).toString();
-                sb.append(subType
-                        .replace("\"", "")
-                        .replace(" ", "_")
-                        .replace("’s", "'s")
-                        .replace("-", "_")).append(",");
-            }
-            final String typeValues = sb.toString().substring(0, sb.toString().length() - 1);
-            this.subTypes = typeValues;
-        }
-    }
-
-    public String getEffectText() {
-        return effectText;
-    }
-    
-    private void extractEffectText(final JsonObject json) {
-        this.effectText = json.get("text").getAsString()
-                .replaceAll("^\\(.+?\\)\n", "")
-                .replaceAll("^.+— ", "")
-                .replace("\n", "~")
-                .replaceAll("~.+? — ", "~")
-                .replaceAll(" \\(.+?\\)", "")
-                .replaceAll("\n~•", "~•")
-                .replaceFirst("~•", " (1)")
-                .replaceFirst("~•", " (2)")
-                .replaceFirst("~•", " (3)")
-                .replaceFirst("~•", " (4)")
-                .replace(getCardName(), "SN");
-    }
-
-    public String getAbilityText() {
-        return abilityText;
-    }
-        
-    private void extractAbilityText(final JsonObject json) {
-        this.abilityText = json.get("text").getAsString()
-                .replaceAll("^\\(.+?\\)\n", "")
-                .replaceAll("^.+— ", "")
-                .replace("\n", ";\\\n        ")
-                .replace(";\\\n        •", " \\\n        •")
-                .replaceAll(" \\(.+?\\)", "")
-                .replaceFirst(" •", " (1)")
-                .replaceFirst(" •", " (2)")
-                .replaceFirst(" •", " (3)")
-                .replaceFirst(" •", " (4)")
-                .replace(getCardName(), "SN");
-    }
-
-    public String getOracleText() {
-        return oracleText;
-    }
-
-    private void extractOracleText(final JsonObject json) {
-        if (json.has("text")) {
-            this.oracleText = json.get("text").getAsString()
-                    .replaceAll("^\\(.+?\\)\n", "")
-                    .replaceAll(" \\(.+?\\)", "")
-                    .replaceAll("\n","\\\\n")
-                    .replaceAll(";",",");
-        }
-    }
-
-    public String getTiming() {
-        if (type.contains("Instant")) {
-            return "removal";
-        } else if (type.contains("Land")){
-            return "land";
-        } else if (subTypes.contains("Equipment")) {
-            return "equipment";
-        } else if (type.contains("Artifact")) {
-            return "artifact";
-        } else if (subTypes.contains("Aura")) {
-            return "aura";
-        } else if (type.contains("Enchantment")) {
-            return "enchantment";
-        } else return "main";
-    }
-
-    private void extractImageUrl(final JsonObject json) throws UnsupportedEncodingException {
-
-        if (json.has("number")) {
-            this.imageUrl = String.format(
-                    "http://magiccards.info/scans/en/%s/%s.jpg",
-                    setCode.toLowerCase(),
-                    json.get("number").getAsString()
-            );
-            clearCardImageError(this);
-
-        } else if (json.has("multiverseid")) {
-            this.imageUrl = setCode.toLowerCase();
-            clearCardImageError(this);
-
-        } else {
-            addCardImageError(this, String.format("%s has no number or multiverseid - cannot set {image} property.", cardName));
-        }
-        
-    }
-    
-    private static void addCardImageError(final CardData card, final String errorDetails) {
-        final String key = card.getCardName();        
-        if (!cardImageErrors.containsKey(key)) {
-            cardImageErrors.put(key, errorDetails);
-        }
-    }
-
-    private static void clearCardImageError(final CardData card) {
-        final String key = card.getCardName();
-        if (cardImageErrors.containsKey(key)) {
-            cardImageErrors.remove(key);
-        }
-    }
-
-    public static String getId(final JsonObject card) {
-        return card.get("name").getAsString().trim();
-    }
-
-    public static String getRarity(final JsonObject card) {
-        return card.get("rarity").getAsString().substring(0, 1);
-    }
-    
-    public static boolean isValid(final JsonObject jsonCard) {
-        return !CardData.getRarity(jsonCard).contentEquals("S") &&
-               (jsonCard.has("number") || jsonCard.has("multiverseid"));
-    }
-
-    public boolean hasSubType() {
-        return getSubType() != null;
+    public boolean hasAbilityText() {
+        return getText() != null && !hasEffectText() && getAbilityText() != null;
     }
 
     public boolean hasColor() {
         return !hasManaCost() && getColor() != null;
     }
 
+    public boolean hasEffectText() {
+        return getText() != null && getEffectText() != null;
+    }
+
     public boolean hasManaCost() {
         return getManaCost() != null;
+    }
+
+    public boolean hasOracleText() {
+        return getText() != null;
     }
 
     public boolean hasPT() {
         return getPower() != null && getToughness() != null;
     }
 
-    public boolean hasEffectText() {
-        return getText() != null && getEffectText() != null;
+    public boolean hasSubType() {
+        return getSubType() != null;
     }
 
-    public boolean hasAbilityText() {
-        return getText() != null && !hasEffectText() && getAbilityText() != null;
-    }
-
-    public boolean hasOracleText() {
-        return getText() != null;
+    public void setText(final String text) {
+        this.text = text;
     }
 
 }
